79d1cd718c71358589b29ee56294f0b4eafa0ed3
Indra Lukmana <indra.lukmana.il@gmail.com>
2018-07-24 12:18:30 +0700

    Add form for editing Note

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -1,25 +1,52 @@
 import React, {Component} from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
 
     constructor(props){
         super(props)
+
+        this.state = {
+            editing: false
+        }
+
         this.edit = this.edit.bind(this)
         this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+
+        this.renderDisplay = this.renderDisplay.bind(this)
+        this.renderForm = this.renderForm.bind(this)
     }
 
 
     edit() {
-        alert('editing note')
+        this.setState({
+            editing: true
+        })
     }
 
     remove() {
         alert('removing note')
     }
 
-    render() {
+    save() {
+        alert('saving note')
+    }
+
+    renderForm(){
+        return(
+            <div className="note">
+                <form>
+                    <textarea/>
+                    <button onClick={this.save}><FaFloppyO /></button>
+                </form>
+            </div>
+        )
+    }
+
+    renderDisplay() {
         return(
             <div className="note">
 
@@ -32,6 +59,10 @@ class Note extends Component {
 
             </div>
         )
+    }
+
+    render(){
+        return this.state.editing ? this.renderForm() : this.renderDisplay()
     }
 }
 
